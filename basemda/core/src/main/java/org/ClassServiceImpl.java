f99c6f9523d9c4293f7a4d3b4973fd9787c61b9f
Francois Le Fevre MIB <flf.mib@gmail.com>
2016-02-24 14:08:08 -0800

    cleaning stuff
    Change-Id: If9692645805a5d644386d05ce1ad031fbd93b906
    Signed-off-by: Francois Le Fevre MIB <flf.mib@gmail.com>

---
+++ basemda/core/src/main/java/org/ClassServiceImpl.java
@@ -0,0 +1,28 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ * TEMPLATE:    SpringServiceImpl.vsl in andromda-spring cartridge
+ * MODEL CLASS: RSM75NewModel::org::ClassService
+ * STEREOTYPE:  Service
+ */
+package org;
+
+/**
+ * @see org.ClassService
+ */
+public class ClassServiceImpl
+    extends ClassServiceBase
+{
+
+    /**
+     * @see org.ClassService#getCompute()
+     */
+    protected  long handleGetCompute()
+        throws Exception
+    {
+        // TODO implement protected  long handleGetCompute()
+        throw new UnsupportedOperationException("org.ClassService.handleGetCompute() Not implemented!");
+    }
+
+}
